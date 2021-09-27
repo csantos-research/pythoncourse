063836d25fa105bed4475c57a1a055bfef9a4b2a
csantos-research <47633152+csantos-research@users.noreply.github.com>
2021-09-27 20:45:20 +0200

    Revised the "qcontrol" script. Removed "newprotocol" Created an empty file to append the results to (results.csv) and start building a database from.

--- Test Data/newprotocol.py
+++
@@ -1,2 +0,0 @@
-import pandas
-print('pandas: {}'.format(pandas.__version__))
--- Test Data/qcontrol.py
+++ Test Data/qcontrol.py
@@ -1,6 +1,47 @@
 import pandas as pd
+import sis
 
+#Prints a welcome message to the user
+print("Welcome to the Quality Index assesment") , sys.argv[0]
+
+#Imports the csv_file in a readable way
 csv_file = pd.read("testdata1.csv")
-csv_file
+df = pd.DataFrame(csv_file)
 
-print("Hello")
+# Creates a series from the CSV test data displaying the first column
+s = df["Insulin"]
+se = pd.Series(s)
+
+#Prints out the column data
+se
+
+#Appends the content of the test file to a "Results" file
+
+dr = pd.DataFrame(results)
+
+dr["New Data"] = csv_file
+
+#Calculates the quality index of the experiment
+
+df["Insulin"].describe()
+quality = min(csv_file/max(csv_file))
+
+print (min(csv_file))
+print (max(csv_file))
+
+def  qresult:
+    if quality < 3:
+        print ("Good job!")
+    else:
+        print ("Try again :(")
+
+print ("The quality index is" , quality , qresult )
+
+#Finally, we plot the data
+
+import matplotlib.pyplot as plt
+
+fig, axs = plt.subplots( figsize=(16, 5))
+df.plot (ax=axs)
+axs.set_ylabel("Insulin in ng/min")
+fig.savefig("Insulin secretion.png")
